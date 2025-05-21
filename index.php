0083f20232509dee0c45bf7c16373b0a740bb002
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-05-21 17:29:44 +0000

    innit

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
       <br />
       <!-- Accent-colored raised button with ripple -->
       <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-        onclick="myButtonClicked()" type="button">
+        type="submit">
         Click here!
       </button>
       </form>
